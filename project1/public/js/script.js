f9405684bc73e1254cbbf0b997d070f2198fb852
sriram-kavadi <kavadi.sriram@gmail.com>
2025-11-03 12:44:37 +0530

    fixed the validation errors in listing/new root

---
+++ project1/public/js/script.js
@@ -0,0 +1,24 @@
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+(() => {
+  'use strict'
+
+  // Fetch all the forms we want to apply custom Bootstrap validation styles to
+  const forms = document.querySelectorAll('.needs-validation')
+
+  // Loop over them and prevent submission
+  Array.from(forms).forEach(form => {
+    form.addEventListener('submit', event => {
+        const price=document.querySelector("#price");
+        if(price<0){
+            alert("Invalid price");
+        }
+      if (!form.checkValidity()) {
+        event.preventDefault()
+        event.stopPropagation()
+      }
+
+      form.classList.add('was-validated')
+    }, false)
+  })
+  
+})()
